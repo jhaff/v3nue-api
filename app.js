27626455dfea994e34510c746e33dafc3502dc8d
jhaff <jacobhaff2@gmail.com>
2018-10-29 15:09:52 -0700

    show all events for a particular venue

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const app = express();
-const venuesRouter = require(./routers/venues)
+// const venuesRouter = require(./routers/venues)
 const port = process.env.PORT || 3000;
 
+
+var venues = {
+   "name" : "Venue",
+   "venue_description" : "Cool venue",
+   "events" : ["dumb event", "event 2", "good event"]
+}
+
+app.get("/venues", (req, res) => {
+   res.json(venues)
+})
+
+//SHOW All events for one venue
+app.get("/venues/:venue_name/events", (req,res) => {
+
+    if (req.params.venue_name === venues.name) {
+        res.json(venues.events)
+    }
+    else{
+        console.log("shit.")
+    }
+
+
+})
+
+
+
 app.listen(port, () => {
-    console.log('App Listening on port 3000');
+   console.log('App Listening on port 3000');
 });
